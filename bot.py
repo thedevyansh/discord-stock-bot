a686a1b8b7296addd014f205467e266f04ccca94
thedevyansh <schawla333333@gmail.com>
2020-12-15 23:00:18 +0530

    Configured bot to send stock details of a specific company as per the user command

--- bot.py
+++ bot.py
@@ -7,6 +7,9 @@ import discord
 # 1
 from discord.ext import commands
 
+top_stock_companies = ['AAPL', 'GOOGL', 'MSFT', 'AMZN', 'FB', 'BRK-B', 'SPY',
+                       'BABA', 'JPM', 'WMT', 'V', 'T', 'UNH', 'PFE', 'INTC', 'VZ', 'ORCL']
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
@@ -14,27 +17,34 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
 
-@bot.event
-async def on_ready():
+async def send_stock_details():
+    global top_stock_companies
+    top_stock_company = random.choice(top_stock_companies)
 
-    print(f'{bot.user.name} has connected to Discord!')
+    top_stock_company_df = yf.download(top_stock_company, period="1d")
 
-    tsla_df = yf.download("MSFT", period="1d")
-    # print(tsla_df.head(1))
+    date = str(top_stock_company_df.head(1).index[0]).split(' ')[0]
     msg = '''\
-        TSLA EOD Data
+        {company} EOF Data
+        - Date: {Date}
         - Open: {Open}
         - High: {High}
         - Low: {Low}
         - Close: {Close}
         - Adj Close: {Adj_Close}
         - Volume: {Volume}\
-     '''.format(Open=tsla_df.iat[0, 0], High=tsla_df.iat[0, 1], Low=tsla_df.iat[0, 2], Close=tsla_df.iat[0, 3], Adj_Close=tsla_df.iat[0, 4], Volume=tsla_df.iat[0, 5])
+     '''.format(company=top_stock_company, Date=date, Open=top_stock_company_df.iat[0, 0], High=top_stock_company_df.iat[0, 1], Low=top_stock_company_df.iat[0, 2], Close=top_stock_company_df.iat[0, 3], Adj_Close=top_stock_company_df.iat[0, 4], Volume=top_stock_company_df.iat[0, 5])
 
     existing_channel = discord.utils.get(
         bot.guilds[0].channels, name='test-channel')
 
     await existing_channel.send(msg)
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+    await send_stock_details()
 
 
 @bot.command(name="99", help='Responds with a random quote from Brooklyn 99.')
@@ -63,6 +73,27 @@ async def roll(ctx, number_of_dice: int, number_of_sides: int):
 
     await ctx.send(', '.join(dice))
 
+@bot.command(name="get_stock_data", help="Gets stock data of a specific company.")
+async def stock_data(ctx, stock_company):
+
+    if stock_company in top_stock_companies:
+        stock_company_df = yf.download(stock_company, period="1d")
+
+        date = str(stock_company_df.head(1).index[0]).split(' ')[0]
+        msg = '''\
+            {company} EOF Data
+            - Date: {Date}
+            - Open: {Open}
+            - High: {High}
+            - Low: {Low}
+            - Close: {Close}
+            - Adj Close: {Adj_Close}
+            - Volume: {Volume}\
+         '''.format(company=stock_company, Date=date, Open=stock_company_df.iat[0, 0], High=stock_company_df.iat[0, 1], Low=stock_company_df.iat[0, 2], Close=stock_company_df.iat[0, 3], Adj_Close=stock_company_df.iat[0, 4], Volume=stock_company_df.iat[0, 5])
+
+        await ctx.send(msg)    
+    else:
+        await ctx.send("Stock data for {stockCompany} doesn't exist!".format(stockCompany=stock_company))
 
 @bot.command(name="create-channel", help="An admin creates a new channel.")
 @commands.has_role('admin')
@@ -80,5 +111,7 @@ async def create_channel(ctx, channel_name='test-channel'):
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
         await ctx.send('You do not have the correct role for this command.')
+    elif isinstance(error, commands.errors.MissingRequiredArgument):
+        await ctx.send('Please enter correct usage.')
 
 bot.run(TOKEN)
