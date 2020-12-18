9eee397d31544ab14cc04bd593361196f6b54af8
thedevyansh <schawla333333@gmail.com>
2020-12-18 22:10:03 +0530

    Added: functionality to show historical stock data

--- sub_bot.py
+++ sub_bot.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 import discord
 import plotly.express as px
+import matplotlib.pyplot as plt
 
 
 async def send_daily_trade_updates_plot(top_stock_company, existing_channel):
@@ -17,3 +18,21 @@ async def send_daily_trade_updates_plot(top_stock_company, existing_channel):
 
     await existing_channel.send(file=discord.File('images/daily_trade_updates_plot_1.png'))
     await existing_channel.send(file=discord.File('images/daily_trade_updates_plot_2.png'))
+
+
+async def send_history_plot(stock_companies, existing_channel):
+    df = yf.download(stock_companies[0])
+    ax = df.plot(y='Close', label=stock_companies[0])
+
+    for stock_company in stock_companies:
+        if stock_company != stock_companies[0]:
+            df = yf.download(stock_company)
+            df.plot(ax=ax, y='Close', label=stock_company)
+
+    plt.xlabel('Date')
+    plt.ylabel('Close')
+    plt.title("Historical stock details")
+
+    plt.savefig('images/history.png')
+
+    await existing_channel.send(file=discord.File('images/history.png'))
