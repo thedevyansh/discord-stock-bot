229aa07b344526c76b2db34af6dc6ed39c132148
thedevyansh <schawla333333@gmail.com>
2021-01-04 00:52:05 +0530

    So two of the issues resolved by using matplotlib and not plotly

--- sub_bot.py
+++ sub_bot.py
@@ -13,25 +13,27 @@ async def send_daily_trade_updates_plot(top_stock_company, existing_channel):
     top_stock_company_df = yf.download(
         top_stock_company, period="1d", interval="1m")
 
-    fig = px.line(top_stock_company_df, y='Close',
-                  title='Latest stock prices of {company}'.format(company=top_stock_company))
-    fig.write_image('images/daily_trade_updates_plot_1.png')
+    top_stock_company_df.plot(y='Close')
 
-    fig = px.line(top_stock_company_df, y=['Open', 'High', 'Low', 'Close', 'Adj Close'],
-                  title='Latest stock prices of {company}'.format(company=top_stock_company))
-    fig.write_image('images/daily_trade_updates_plot_2.png')
+    plt.xlabel('Datetime')
+    plt.ylabel('Close')
+    plt.title('Latest stock prices of {company}'.format(company=top_stock_company))
+    plt.savefig('images/daily_trade_updates_plot_1.png')
 
-    # my_files = [
-    #     discord.File('images/daily_trade_updates_plot_1.png'),
-    #     discord.File('images/daily_trade_updates_plot_2.png')
-    # ]
+    top_stock_company_df.plot(y=['Open', 'High', 'Low', 'Close', 'Adj Close'])
 
-    await existing_channel.send(file=discord.File('images/daily_trade_updates_plot_2.png'))
+    plt.xlabel('Datetime')
+    plt.ylabel('Value')
+    plt.title('Latest stock prices of {company}'.format(company=top_stock_company))
+    plt.savefig('images/daily_trade_updates_plot_2.png')
 
-    #await existing_channel.send('Latest stock prices:', files=my_files)
 
-    # await existing_channel.send(file=discord.File('images/daily_trade_updates_plot_1.png'))
-    # await existing_channel.send(file=discord.File('images/daily_trade_updates_plot_2.png'))
+    my_files = [
+        discord.File('images/daily_trade_updates_plot_1.png'),
+        discord.File('images/daily_trade_updates_plot_2.png')
+    ]
+
+    await existing_channel.send('Latest stock prices:', files=my_files)
 
 
 async def send_history_plot(stock_companies, existing_channel):
